83edddb515a261c6f688e93dc7d4a38c58dcb56d
t4zdi0Neo <js.ortegar1@uniandes.edu.co>
2023-11-25 20:41:29 -0500

    Time!

--- PlatformIO/CarBoard/lib/CarBoardREST-master/CarBoardREST.h
+++ PlatformIO/CarBoard/lib/CarBoardREST-master/CarBoardREST.h
@@ -5,19 +5,33 @@
 
 // JSON data buffer
 StaticJsonDocument<1200> jsonDocument;
+StaticJsonDocument<600> timeDocument;
 char buffer[1200];
 
 Preferences* _preferences;
 
 //Funciones de manejo JSON Auxiliares
-void create_json(float node, float latitude, float longitude, float speed, float acce) {  
+void create_json(float node, float latitude, float longitude, uint32_t speed, float acce, int azimuth, uint16_t positionDOP,
+                 uint16_t year, uint8_t month, uint8_t day, uint8_t hour, uint8_t minute, uint8_t second){  
   jsonDocument.clear();
+  timeDocument.clear(); 
+
   jsonDocument["nodo"] = node;
   jsonDocument["latitud"] = latitude;
   jsonDocument["longitud"] = longitude;
   jsonDocument["velocidad"] = speed;
   jsonDocument["aceleracion"] = acce;
- 
+  jsonDocument["azimuth"] = azimuth;
+  jsonDocument["positionDOP"] = positionDOP;
+
+  timeDocument["year"] = year;
+  timeDocument["month"] = month;
+  timeDocument["day"] = day;
+  timeDocument["hour"] = hour;
+  timeDocument["minute"] = minute;
+  timeDocument["second"] = second;
+
+  jsonDocument["time"] = timeDocument;
 }
 
 
@@ -26,12 +40,22 @@ namespace CarBoardREST{
     WebServer* _serverPointer;
     int _bufferSize;
 
-    float *_latitude;
-    float *_longitude;
-    float *_azimuth;
+    int32_t *_latitude;
+    int32_t *_longitude;
+    int *_azimuth;
 
     float *_acceleration;
-    float *_speed;
+    uint32_t *_speed;
+
+    uint16_t *_positionDOP;
+    
+
+    uint16_t *_year;
+    uint8_t *_month;
+    uint8_t *_day;
+    uint8_t *_hour;
+    uint8_t *_minute;
+    uint8_t *_second;
    
 
     void setBufferSize(int bufferSize){
@@ -42,27 +66,41 @@ namespace CarBoardREST{
         _serverPointer = serverPointer;
     }
 
-    void linkAzimuth(float* azimuth){
+    void linkAzimuth(int* azimuth){
         _azimuth = azimuth;
     }
 
    void linkAcce(float* acce){
         _acceleration = acce;
     }
-   void linkSpeed(float* speed){
+   void linkSpeed(uint32_t* speed){
         _speed = speed;
     }
-   void linkLatitude(float* latitude){
+   void linkLatitude(int32_t* latitude){
         _latitude = latitude;
     }
-   void linkLongitude(float* longitude){
+   void linkLongitude(int32_t* longitude){
         _longitude = longitude;
     }
+
+    void linkPositionDOP(uint16_t* positionDOP){
+          _positionDOP = positionDOP;
+     }
+
+
+     void linkTime(uint16_t* year, uint8_t* month, uint8_t* day, uint8_t* hour, uint8_t* minute, uint8_t* second){
+          _year = year;
+          _month = month;
+          _day = day;
+          _hour = hour;
+          _minute = minute;
+          _second = second;
+     }
 
     //Métodos POST
     char* postContent(){
       jsonDocument.clear();
-      create_json(1, (*_latitude), (*_longitude), (*_speed), (*_acceleration));
+      create_json(1, (*_latitude), (*_longitude), (*_speed), (*_acceleration), (*_azimuth), (*_positionDOP), (*_year), (*_month), (*_day), (*_hour), (*_minute), (*_second));
       serializeJson(jsonDocument, buffer); 
       return buffer;
     }
--- PlatformIO/CarBoard/src/main.cpp
+++ PlatformIO/CarBoard/src/main.cpp
@@ -6,12 +6,48 @@
 #include <ConnecT.h>
 #include <CarBoardREST.h>
 #include <QMC5883LCompass.h>
-#include "SparkFun_Ublox_Arduino_Library.h" 
+#include <SparkFun_u-blox_GNSS_Arduino_Library.h> 
 #include <HTTPClient.h>
-SFE_UBLOX_GPS gps;
+#include <WiFiClientSecure.h>
+SFE_UBLOX_GNSS gps;
 
 
-const char* serverName = "http://192.168.1.2:8080/measurement/log/";
+const char* rootCACertificate = \ 
+"-----BEGIN CERTIFICATE-----\n" \
+"MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw\n" \
+"TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh\n" \
+"cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4\n" \
+"WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu\n" \
+"ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY\n" \
+"MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc\n" \
+"h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+\n" \
+"0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U\n" \
+"A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW\n" \
+"T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH\n" \
+"B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC\n" \
+"B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv\n" \
+"KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn\n" \
+"OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn\n" \
+"jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw\n" \
+"qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI\n" \
+"rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV\n" \
+"HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq\n" \
+"hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL\n" \
+"ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ\n" \
+"3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK\n" \
+"NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5\n" \
+"ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur\n" \
+"TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC\n" \
+"jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc\n" \
+"oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq\n" \
+"4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA\n" \
+"mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d\n" \
+"emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=\n" \
+"-----END CERTIFICATE-----\n";
+
+
+
+const char* serverName = "https://carboard.lat/measurement/log/";
 unsigned long lastTime_serve = 0;
 unsigned long timerDelay_serve = 5000;
 
@@ -26,17 +62,27 @@ unsigned long timerDelay_serve = 5000;
 #define GPS_BAUDRATE 9600
 
 // ### Variables ### //
-float latitude = 0;
-float longitude = 0;
+int32_t latitude = 0;
+int32_t longitude = 0;
 
 double acce_x = 0;
 double acce_y = 0;
 double acce_z = 0;
 float acceleration = 0;
 
-float azimuth = 0;
+int azimuth = 0;
 
-float speed = 0;
+uint32_t speed = 0;
+
+uint16_t position_dop = 0;
+uint32_t weekTime = 0;
+
+uint16_t year = 0;
+uint8_t month = 0;
+uint8_t day = 0;
+uint8_t hour = 0;
+uint8_t minute = 0;
+uint8_t second = 0;
    
 
 // ### Objetos ### //
@@ -58,17 +104,13 @@ void setup() {
 
   //# MPU config
   
-  if (!mpu.begin()) {
+  while (!mpu.begin()) {
     Serial.println("No se encontró el MPU!");
-    while (1) {
-      delay(10);
-    }
   }
 
   //# Screen config
-  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+  while(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
     Serial.println(F("No se encontró la pantalla!"));
-    for(;;); // Don't proceed, loop forever
   }
 
   pinMode(LED_BUILTIN, OUTPUT);
@@ -85,6 +127,7 @@ void setup() {
   // ## Inicialización del WiFi
   connecT.setDualMode();
   connecT.setWiFi_AP("CarBoard", "carboard");
+  connecT.setWiFi_STA("COMPETENCE", "Mafu2408");
   connecT.setWebServer(80); // Creación del servidor web en el puerto 80
 
   /* Se vinculan los apuntadores de los arrays de medición al protocolo REST */
@@ -96,15 +139,20 @@ void setup() {
   CarBoardREST::linkLatitude(&latitude);
   CarBoardREST::linkLongitude(&longitude);
 
+  CarBoardREST::linkPositionDOP(&position_dop);
+  CarBoardREST::linkTime(&year, &month, &day, &hour, &minute, &second);
 
   display.clearDisplay();
   display.display();
 
   display.setTextSize(1);
   display.setTextColor(SSD1306_WHITE);
+
   display.setCursor(0,0);
-  display.print("Acce X: ");
-  display.print(acce_x);
+  display.print("Lat: ");
+
+  display.setCursor(0,10);
+  display.print("Lon: ");
   display.display(); // 
 
 
@@ -124,8 +172,24 @@ void setup() {
 }
 
 void loop() {
+
+
   // Loop Led
   digitalWrite(LED_BUILTIN, HIGH);
+
+  // Lectura del GPS
+  latitude = gps.getLatitude();
+  longitude = gps.getLongitude();
+  speed = gps.getGroundSpeed();
+  position_dop = gps.getPositionDOP();
+
+  //Tiempo del GPS
+  year = gps.getYear();
+  month - gps.getMonth();
+  day = gps.getDay();
+  hour = gps.getHour();
+  minute = gps.getMinute();
+  second = gps.getSecond();
 
   // Lectura del compás
   compass.read();
@@ -139,35 +203,81 @@ void loop() {
   mpu_accel->getEvent(&accel);
   mpu_gyro->getEvent(&gyro);
 
+  acce_x = accel.acceleration.x;
+  acce_y = accel.acceleration.y;
+  acce_z = accel.acceleration.z;
+  acceleration = sqrt(pow(acce_x, 2) + pow(acce_y, 2) + pow(acce_z, 2));
+
 
 
   //Listen requests
   (connecT.getServerPointer())->handleClient();
 
-
+  
   //POST TO SERVER
   if ((millis() - lastTime_serve) > timerDelay_serve) {
 
+    //Secure
+    WiFiClientSecure *client = new WiFiClientSecure;
+      if(client) {
+      // set secure client with certificate
+      client->setCACert(rootCACertificate);
+      //create an HTTPClient instance
+      }
 
-    WiFiClient client;
-    HTTPClient http;
+    HTTPClient https;
 
-    http.begin(client, serverName);
-    http.addHeader("Content-Type", "application/json");
+    https.begin(*client, serverName);
+    https.addHeader("Content-Type", "application/json");
     String httpRequestData = CarBoardREST::postContent();           
-    int httpResponseCode = http.POST(httpRequestData);
+    int httpResponseCode = https.POST(httpRequestData);
     
 
     Serial.print("HTTP Response code: ");
     Serial.println(httpResponseCode);
       
     // Free resources
-    http.end();
+
     lastTime_serve = millis();
     }
 
 
+  //Gráfica de la pantalla
+
+  display.clearDisplay();
+
+  display.setCursor(0,0);
+  display.print("Lat: ");
+  display.print(latitude);
+
+  display.setCursor(0,10);
+  display.print("Lon: ");
+  display.print(longitude);
+
+  display.setCursor(0,20);
+  display.print("Az: ");
+  display.print(azimuth);
+
+  display.setCursor(0,30);
+  display.print("Acce: ");
+  display.print(acceleration);
+
+
+  display.setCursor(0,40);
+  display.print("Speed: ");
+  display.print(speed);
+
+
+  display.setCursor(0,50);
+  display.print("Time: ");
+  display.print(weekTime);
+
+
+
   display.display();
+  Serial.println("Loop");	
+
+
   }
 
 
